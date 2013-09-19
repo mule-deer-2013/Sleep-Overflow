435be87453a1a494a4b59bf747a1ee300ab056df
Jimmy <jimwong1023@gmail.com>
2013-09-18 17:33:13 -0700

    add question feature test and question routes for show, new create.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,14 @@
 class QuestionsController < ApplicationController
   def new
     @question = Question.new
+    # TODO: NEED USER OBJECT
+    # @user = current_user
+  end
+
+  def create
+    question = Question.new(params[:question])
+    puts params[:question]
+    question.save
   end
 
 end
--- spec/features/add_questions_spec.rb
+++ spec/features/add_questions_spec.rb
@@ -8,5 +8,16 @@ feature 'User can add questions' do
       find_field("question_content")
       find_button("Create Question")
     end
+
+    it "creates question" do
+      visit new_question_path
+
+      expect {
+      fill_in "question_title", :with => "Do I have a question?"
+      fill_in "question_content", :with => "Yes, you do."
+      click_button 'Create Question'
+      }.to change(Question, :count).by(1)
+    end
+
   end
 end   
