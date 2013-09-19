66e992cfee657b4abcf77a69553b199bbe3b5dc2
Dok <dgurson@gmail.com>
2013-09-19 16:07:27 -0700

    final changes before merging

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,5 +2,10 @@ class VotesController < ApplicationController
 	def create
 		vote = Vote.create(params[:form])
 		redirect_to question_path(vote.votable.question)
+
+		# to add AJAX ...if you want to
+		# 1. Instead of redirect_to, use render json: { score: vote.votable.score }
+		# 2. Add 'remote: true' to the options after controller, action, and before form in the button_to
+		# 3. Add some Javascript code that listens for an 'ajax:success' event to update the DOM with the new score
 	end
 end
