ada331e4e10db4c7817253dfaf960cab00e87254
Apprentice <apprentice@dbc21.local>
2013-09-19 17:40:29 -0700

    tests

--- spec/features/profile_page_spec.rb
+++ spec/features/profile_page_spec.rb
@@ -1,9 +1,39 @@
 require 'spec_helper'
 
-feature 'profile page'  do
-  it "displays the users answers" do
+
+
+describe "before logging in" do
+
+
+  it "does NOT display the logout button when user has not logged in" do
     visit '/'
-    click_button 'my profile page'
+    page.should_not have_selector('navbar_logout')
+  end
+
+  it "displays the signin button when user has not logged in" do
+    visit '/'
+    page.should have_button('signin')
+  end
+
+end
+
+describe 'after logging in' do
+  before(:each) do 
+    User.create!(username:'jimmy', password:'jimmy',email:'jimmy')
+    visit '/'
+    fill_in('username', :with => 'jimmy')
+    fill_in('password', :with => 'jimmy')
+    click_on('signin')
+  end
+
+
+  it "displays the logout button when user has logged in" do
+      page.should have_button('logout')
+  end
+
+it "displays the signin button after logging out" do
+    click_on('logout')
+    page.should have_button('signin')
   end
 end
 
