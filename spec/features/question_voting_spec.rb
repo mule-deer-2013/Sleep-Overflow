e9577b7d43c225f36fc1c5bdff56c0c7acbc771d
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-20 23:54:24 -0700

    fix question voting tests

--- spec/features/question_voting_spec.rb
+++ spec/features/question_voting_spec.rb
@@ -14,14 +14,14 @@ let!(:question) { FactoryGirl.create :question, user_id: user.id }
       fill_in "Password", with: "password"
       click_button "signin"
       visit question_path(question)
-      page.should have_button("Upvote")
-      page.should have_button("Downvote")
+      page.should have_button("upvote")
+      page.should have_button("downvote")
     end
 
     it "if not logged in" do
       visit question_path(question)
-      page.should_not have_button("Upvote")
-      page.should_not have_button("Downvote")
+      page.should_not have_button("upvote")
+      page.should_not have_button("downvote")
     end
 
     it "including number of votes" do
@@ -41,27 +41,27 @@ let!(:question) { FactoryGirl.create :question, user_id: user.id }
     end
 
     it "cannot upvote same question twice on a question" do
-      click_button "Upvote"
-      expect { click_button "Upvote" }.to_not change{ Vote.count }.by(1)
+      click_button "upvote"
+      expect { click_button "upvote" }.to_not change{ Vote.count }.by(1)
     end
 
     it "cannot downvote same question twice" do
-      click_button "Downvote"
-      expect { click_button "Downvote" }.to_not change{ Vote.count }.by(1)
+      click_button "downvote"
+      expect { click_button "downvote" }.to_not change{ Vote.count }.by(1)
     end
 
     it "can see a change in the vote count" do
-      expect { click_button "Upvote" }.to change{ question.score }.by(1)
+      expect { click_button "upvote" }.to change{ question.score }.by(1)
     end
 
     it "can switch from downvote to upvote" do
-      click_button "Downvote"
-      expect { click_button "Upvote" }.to change{ question.score }.by(2)
+      click_button "downvote"
+      expect { click_button "upvote" }.to change{ question.score }.by(2)
     end
 
     it "can unvote" do 
-      click_button "Upvote"
-      expect { click_button "Upvote" }.to change{ question.score }.by(-1)
+      click_button "upvote"
+      expect { click_button "upvote" }.to change{ question.score }.by(-1)
     end
 
   end
